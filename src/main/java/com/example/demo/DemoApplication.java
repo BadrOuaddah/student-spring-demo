f521fe3457484b05d7f234d56eb0c701c7592d3a
Badr Ouaddah <119801735+BadrOuaddah@users.noreply.github.com>
2022-12-31 19:56:16 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -19,15 +19,4 @@ public class DemoApplication {
     }
 }
 
-// TODO: add Angular App to API spring and start to learn front-end with Angular framework and add to spring
-//https://www.youtube.com/watch?v=rdLJNGZvlAA&ab_channel=TechieOcean
-//https://www.youtube.com/watch?v=9nxSvecSa50&ab_channel=CodeStepByStep
-//StudentComponent
-//First task display the List
-//Insert a student
-//Delete a student
-//Update a student
-//https://www.youtube.com/watch?v=yPqusXA5FNg&ab_channel=JavaTechie
-//Youtube search : How to consume simple Api Angular
-//localhost:8080
-//https://www.youtube.com/watch?v=OJ7-EC69LfU&ab_channel=CodeStepByStep
+
